0334155a5c0f76f356297087cfd4c5cdd908356b
quietshu <ds303077135@gmail.com>
2014-12-25 07:38:54 +0800

    Touch & fixes.

--- src/listlabel.js
+++ src/listlabel.js
@@ -14,20 +14,20 @@
          * The selector fn.
          * @param element [array of] DOM element or string
          */
-        var selector = function (element) {
+        var selector = function (element, options) {
 
             if (isElement(element)) {
-                return initListlabel(element);
+                return initListlabel(element, options);
             } else if (typeof element === "string") {
                 var array = document.querySelectorAll(element);
 
-                return selector(array);
+                return selector(array, options);
             } else if (typeof element === "object" && (element instanceof Array || element instanceof NodeList)) {
                 var retArray = [];
 
                 // NodeList doesn't has method `forEach`
                 for (var i = 0; i < element.length; ++i) {
-                    retArray.push(selector(element[i]));
+                    retArray.push(selector(element[i], options));
                 }
 
                 return retArray;
@@ -38,8 +38,8 @@
         };
 
         // Defaults
-        selector.defaultListHeight = 300; // px
-        selector.defaultLabelClass = "ll-label";
+        selector.defaultLabelClass  = "ll-label";
+        selector.defaultListHeight  = 300; // px
 
         /**
          * Throw an error.
@@ -66,23 +66,44 @@
 
         }
 
-        function initListlabel(el) {
+        /**
+         * Test for touch devices.
+         * @method isTouchDevice
+         * @see http://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
+         */
+        function isTouchDevice() {
+            try {
+                document.createEvent("TouchEvent");
+                return true;
+            } catch (e) {
+                return false;
+            }
+        }
+
+        function initListlabel(el, options) {
             if (!isElement(el)) {
                 throwError("ERROR: Type error.");
 
                 return undefined;
             }
 
-            var element = el,
-                labels  = [];
+            var element     = el,
+                labels      = [],
+                labelIndexs = [];
 
-            var label = function (el) {
-                var element   = el,
-                    height    = 0,
-                    offsetTop = 0;
+            var label = function (el, width) {
+                var element      = el,
+                    height       = 0,
+                    offsetTop    = 0;
+
+                // Reset styles
+                element.style.width = width + "px";
 
                 // The label closure
                 var labelObject = {
+                    text: function () {
+                        return el.innerText;
+                    },
                     top: function () {
                         if (arguments.length && typeof arguments[0] === "number")
                             element.style.top = Math.floor(arguments[0]) + "px";
@@ -120,7 +141,7 @@
                 labels = [];
 
                 for (var i = 0; i < labelArray.length; ++i) {
-                    labels.push(label(labelArray[i]));
+                    labels.push(label(labelArray[i], element.clientWidth));
                 }
             };
 
@@ -132,8 +153,12 @@
                     return this;
                 },
                 bindScroll: function() {
-                    element.onscroll = element.ontouchmove = function () {
-                        var scrollTop = element.scrollTop;
+                    var refresh = function () {
+                        var scrollTop;
+                        if (arguments.length && typeof arguments[0] === "number")
+                            scrollTop = arguments[0];
+                        else
+                            scrollTop = element.scrollTop;
 
                         for (var i = 0; i < labels.length; ++i) {
                             if (labels[i].top() <= scrollTop) {
@@ -152,21 +177,148 @@
                         }
                     };
 
+                    if (isTouchDevice()) {
+
+                        var sStart = 0,
+                            yStart = 0,
+                            yLast  = 0,
+                            tLast  = 0,
+                            speed  = 0;
+/*
+                        var frame = function () {
+                            var tmp   = element.scrollTop,
+                                delta = speed * (Date.now() - tLast);
+
+                            tmp += delta;
+                            tLast = Date.now();
+
+                            if (tmp <= element.scrollHeight - element.clientHeight)
+                                refresh(tmp);
+                            element.scrollTop = tmp;
+
+                            speed *= 0.9;
+
+                            if (Math.abs(delta) > 5)
+                                requestAnimationFrame(frame);
+                            else {
+                                speed = 0;
+                                tLast = 0;
+                            }
+                        };
+*/
+                        element.ontouchstart = function (event) {
+                            event.preventDefault();
+
+                            sStart = element.scrollTop;
+                            yStart = event.pageY;
+/*                            speed = 0;
+                            yLast = yStart;
+                            tLast = Date.now();*/
+                        };
+
+                        element.ontouchmove = function (event) {
+                            event.preventDefault();
+
+                            var deltaY = yStart - event.pageY + sStart;
+
+                            if (deltaY <= element.scrollHeight - element.clientHeight)
+                                refresh(deltaY);
+                            element.scrollTop = deltaY;
+/*
+                            speed = speed * 0.05 + (event.pageY - yLast) / (Date.now() - tLast) * 0.15;
+                            tLast = Date.now();
+                            yLast = event.pageY; */
+                        };
+/*
+                        element.ontouchend = function (event) {
+                            event.preventDefault();
+
+                            var deltaY = yStart - event.pageY + sStart;
+
+                            if (deltaY <= element.scrollHeight - element.clientHeight)
+                                refresh(deltaY);
+                            element.scrollTop = deltaY;
+
+                            speed = speed * 0.05 + (event.pageY - yLast) / (Date.now() - tLast) * 0.15;
+                            tLast = Date.now();
+                            yLast = event.pageY;
+
+                            speed *= 0.5;
+
+                            requestAnimationFrame(frame);
+                        };
+*/
+                    }
+                    else
+                        element.onscroll = refresh;
+
                     return this;
                 },
-                init: function () {
+                showIndex: function () {
+                    labelIndexs = [];
+                    var indexContainer = document.createElement("UL");
+
+                    for (var i = 0; i < labels.length; ++i) {
+                        var indexElement = document.createElement("LI");
+                        indexElement.innerText = labels[i].inherit().text();
+
+                        // This is a trick for creating a closure
+                        with (labels[i]) {
+                            indexElement.onmousedown = indexElement.onmousemove = indexElement.ontouchstart = indexElement.ontouchmove = function () {
+                                element.scrollTop = top();
+                            };
+                        }
+
+                        labelIndexs.push(indexElement);
+                        indexContainer.appendChild(indexElement);
+                    }
+
+                    // Index container styles
+                    indexContainer.style.position   = "absolute";
+                    indexContainer.style.top        = element.offsetTop + "px";
+                    indexContainer.style.height     = element.offsetHeight - 20 + "px";
+                    indexContainer.style.margin     = "5px";
+                    indexContainer.style.padding    = "5px";
+                    indexContainer.style.zIndex     = (+element.style.zIndex) ? (+element.style.zIndex + 1) : 1;
+                    indexContainer.style.listStyle  = "none";
+                    indexContainer.style.fontSize   = "12px";
+                    indexContainer.style.lineHeight = (element.offsetHeight - 20) / (labelIndexs.length || 1) / 12 + "em";
+                    indexContainer.style.overflow   = "hidden";
+
+                    labelIndexs.forEach(function (indexElement) {
+                        indexElement.style.height   = (element.offsetHeight - 20) / (labelIndexs.length || 1) / 12 + "em";
+                    });
+
+                    element.parentNode.insertBefore(indexContainer, element);
+
+                    indexContainer.style.left       = element.offsetLeft + element.offsetWidth - indexContainer.clientWidth - 30 + "px";
+
+                },
+                init: function (options) {
                     // Container styles
-                    element.style.height                        = selector.defaultListHeight + "px";
-                    element.style.position                      = "relative";
-                    element.style.overflow                      = "scroll";
+                    element.style.position         = "relative";
+                    element.style.height           = selector.defaultListHeight + "px";
+                    element.style.overflowY        = "scroll";
                     element.style["-webkit-overflow-scrolling"] = "touch";
 
+                    this.refreshLabels();
+
+                    if (typeof options !== "undefined") {
+                        if (typeof options.height === "number" && options.height >= 0) {
+                            element.style.height = options.height + "px";
+                        }
+
+                        if (options.showIndex == true) {
+                            this.showIndex();
+                        }
+                    }
+
                     // Get all labels and bind scroll event
-                    return this.refreshLabels().bindScroll();
+                    return this.bindScroll();
                 }
             };
 
-            return listLabelObject.init();
+            return listLabelObject.init(options);
         }
 
         return selector;
